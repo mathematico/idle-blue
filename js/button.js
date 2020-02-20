09fed4fc59e53ab8db9065c899ec7bdc0f7bb503
mathematico <47189734+mathematico@users.noreply.github.com>
2020-02-20 13:58:07 -0500

    zas

--- js/button.js
+++ js/button.js
@@ -1,6 +1,6 @@
 document.addEventListener("mousemove", mouseP, false);
      function mouseP (e) {
-        mouseX = e.clientX-10; 
+        mouseX = e.clientX-10;
         mouseY = e.clientY-10;
          if (Math.floor((mouseX-mapx)/tileW)<mapW && Math.floor((mouseY-mapy)/tileH) <mapH &&mouseX>mapx &&mouseY>mapy)
             {    mouseCase= Math.floor((mouseX-mapx)/tileW)+ Math.floor((mouseY-mapy)/tileH)*mapH;
@@ -8,49 +8,49 @@ document.addEventListener("mousemove", mouseP, false);
             } else{
             mouseCase=null }
      }
-        
+
 document.addEventListener("click",checkbutton);
-    
+
 
 
 
 function drawbutton(){
     if (gamedone == true){
-        
+
         butcreate(450,400,100,50,"gray","restart?")
-        
+
     }
      if (mmenu == true && scrapamount>0){
-        
+
         butcreate(0,540,100,50,"brown","improvements")
-        
+
     }
     if (shopfirst == false && Ssmenu== true){
-        
+
         butcreate(0,540,100,50,"brown","back")
-        
+
     }
     if (shopfirst == false && Ssmenu== true){
-        
+
         butcreate(0,80,200,50,"red","Heal " +Math.round(0.1*S_healthmax)+ " Hp price:"+Healprice)
-        
+
     }
     if (shopfirst == false && Ssmenu== true && Healprice>=8){
-        
+
         butcreate(0,130,200,50,"red","Buy " +Math.round(0.04*S_healthmax)+ " armor price:"+scraparmorprice)
-        
+
     }
     if (shopfirst == false && Ssmenu== true && S_shieldregen>0){
-        
+
         butcreate(0,240,200,50,"royalblue","Increase to " +Math.floor(S_shieldmax * 1.25)+ " Shield max, price:"+Shieldmaxprice,7)
-        
+
     }
     if (shopfirst == false && Ssmenu== true && S_shieldmax>20){
-        
+
         butcreate(0,290,200,50,"royalblue","Increase to " +S_shieldregen*2 + " Shield regen, price:"+Shieldregenprice,7)
-        
+
     }
-    
+
 }
 
 
@@ -71,7 +71,7 @@ function checkbutton(){
             mmenu=false
             bpres =false
             Ssmenu = true
-        }  
+        }
      }
     if (shopfirst == false && Ssmenu== true){
         mcheck(mouseX,mouseY,0,540,100,50,1)
@@ -81,16 +81,16 @@ function checkbutton(){
             mmenu=true
             bpres =false
         }
-        
+
     }
     if (shopfirst == false && Ssmenu== true){
         mcheck(mouseX,mouseY,0,80,200,50,2)
         if (bpres ==true && bid==2){
-            
+
             buyheal()
             bpres = false
         }
-        
+
     }
     if (shopfirst == false && Ssmenu== true && Healprice>=8){
         mcheck(mouseX,mouseY,0,130,200,50,3)
@@ -98,7 +98,7 @@ function checkbutton(){
              buyscraparmor()
             bpres=false
         }
-        
+
     }
     if (shopfirst == false && Ssmenu== true && S_shieldregen>0){
         mcheck(mouseX,mouseY,0,240,200,50,4)
@@ -106,7 +106,7 @@ function checkbutton(){
              buyshieldmax()
             bpres=false
         }
-        
+
     }
     if (shopfirst == false && Ssmenu== true && S_shieldmax>20){
         mcheck(mouseX,mouseY,0,290,200,50,5)
@@ -114,20 +114,20 @@ function checkbutton(){
              buyshieldregen()
             bpres=false
         }
-        
+
     }
-    
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
